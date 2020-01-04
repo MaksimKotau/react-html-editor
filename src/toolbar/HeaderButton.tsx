0fd4dc7195bae3d9cb1f164c856d24f72ca8be3f
MaksimKotau <maxkotov@gmail.com>
2020-01-04 08:31:35 -0500

    Corrections in headerButton

--- src/toolbar/HeaderButton.tsx
+++ src/toolbar/HeaderButton.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Portal from './PortalModal';
+import Dialog from '@material-ui/core/Dialog';
 
 enum HeaderType {
     H1 = "Header 1",
@@ -12,9 +13,8 @@ enum HeaderType {
 
 const HeaderButton: React.FC<{}> = () => {
     const [showModal, setShowModal] = useState<boolean>(false);
-    const [anchorEl, setAnchorEl] = useState<HTMLElement | undefined>(undefined);
     const addHeader = (header: string) => {
-        document.execCommand('formatBlock',false, `<${header}>`)
+        document.execCommand('formatBlock', false, `<${header}>`)
         setShowModal(false);
     }
     return (
@@ -23,61 +23,45 @@ const HeaderButton: React.FC<{}> = () => {
                 <span
                     style={{ display: "flex", flexDirection: "row", flexGrow: 1, justifyContent: "center", fontStyle: "bold" }}
                     onClick={(e) => {
-                        setAnchorEl(e.currentTarget)
                         setShowModal(true)
                     }}
                 >H</span>
             </div>
-            {showModal && <Portal
-                anchorEl={anchorEl!}
+            {showModal && <Dialog
+                open={showModal}
             >
-                <div
-                    style={{
-                        position: "fixed",
-                        width: 200,
-                        height: 280,
-                        top: "calc(50% - 150px)",
-                        left: "calc(50% - 150px)",
-                        boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 12px 0 rgba(0, 0, 0, 0.19)",
-                        padding: 20,
-                        backgroundColor: "white"
-                    }}
-                >
-                    {
-                        Object.values(HeaderType).map((el, i) => {
-                            return (
-                                <div
-                                    className="listElement"
-                                    key={`Header_type_${el}`}
-                                    style={{
-                                        fontSize: 25 - i * 2,
-                                        width: "100%",
-                                        height: 40,
-                                        display: "flex",
-                                        boxSizing: "border-box",
-                                        alignItems: "center  ",
-                                        paddingLeft: 15
-                                    }}
-                                    onClick={() => {addHeader(`h${i + 1}`)}}
-                                    >
-                                    <span style={{ textDecoration: "none" }}>{el}</span>
-                                </div>
-                            )
-                        })
-                    }
-                    <button
+                {
+                    Object.values(HeaderType).map((el, i) => {
+                        return (
+                            <div
+                                className="listElement"
+                                key={`Header_type_${el}`}
+                                style={{
+                                    fontSize: 25 - i * 2,
+                                    width: "100%",
+                                    height: 40,
+                                    display: "flex",
+                                    boxSizing: "border-box",
+                                    alignItems: "center  ",
+                                    paddingLeft: 15
+                                }}
+                                onClick={() => { addHeader(`h${i + 1}`) }}
+                            >
+                                <span style={{ textDecoration: "none" }}>{el}</span>
+                            </div>
+                        )
+                    })
+                }
+                <button
                     style={{
                         padding: "7px 20px",
                         marginTop: 10
                     }}
-                        onClick={() => setShowModal(false)}
-                    >
-                        Cancel
+                    onClick={() => setShowModal(false)}
+                >
+                    Cancel
                     </button>
-                </div>
-
-
-            </Portal>}
+            </Dialog>}
         </>
     )
 }
